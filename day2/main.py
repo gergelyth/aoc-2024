aa25b13a2a9482eb5405cb95ebb8aeb6933259de
gergelyth <gergelytothdev@gmail.com>
2024-12-02 22:22:43 +0100

    [Day2] Part 2

--- day2/main.py
+++ day2/main.py
@@ -4,17 +4,24 @@ from core import test_and_submit
 from util import get_lines
 
 
+def is_valid_sequence(nums):
+    first_diff = nums[1] - nums[0]
+    return all(first_diff * (y-x) > 0 and 1 <= abs(y-x) <= 3 for x, y in zip(nums, nums[1:]))
+
 def solution(input: str) -> tuple[any, any]:
     lines = get_lines(input)
     result = 0
     for line in lines:
         numbers = [int(x) for x in line.split()]
-        first_diff = numbers[1] - numbers[0]
-        is_safe = all([first_diff * (y-x) > 0 and 1 <= abs(y-x) <= 3 for (x,y) in zip(numbers, numbers[1:])])
+        # terrible complexity
+        is_safe = is_valid_sequence(numbers) or any(
+            is_valid_sequence(numbers[:i] + numbers[i+1:])
+            for i in range(len(numbers))
+        )
         if is_safe:
-            result +=1
+            result += 1
 
-    return (result, False)
+    return (None, result)
 
 puzzle = Puzzle(2024, 2)
 test_and_submit(puzzle, solution, False)
