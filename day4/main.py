ce4504c033f8d7569e5a8f843ee27a4da1bd9aff
gergelyth <gergelytothdev@gmail.com>
2024-12-04 23:50:11 +0100

    [Day4] Part 2

--- day4/main.py
+++ day4/main.py
@@ -4,15 +4,15 @@ from aocd.models import Puzzle
 from core import test_and_submit
 from util import Matrix
 
-TARGET = "XMAS"
-BACKWARDS = "SAMX"
+TARGET = "MAS"
+BACKWARDS = "SAM"
 
-def is_target(matrix: Matrix, positions: list[tuple[int, int]]) -> int:
+def is_target(matrix: Matrix, positions: list[tuple[int, int]]) -> bool:
     constructed_word = ""
     for position in positions:
         constructed_word += matrix.get_item_pos(position) or ""
 
-    return int(constructed_word in (TARGET, BACKWARDS))
+    return constructed_word in (TARGET, BACKWARDS)
 
 def solution(input: str) -> tuple[any, any]:
     matrix = Matrix(input)
@@ -20,12 +20,13 @@ def solution(input: str) -> tuple[any, any]:
     result = 0
     for row in range(matrix.row_count):
         for col in range(matrix.col_count):
-            result += is_target(matrix, [(row, col+i) for i in range(4)])
-            result += is_target(matrix, [(row+i, col) for i in range(4)])
-            result += is_target(matrix, [(row+i, col-i) for i in range(4)])
-            result += is_target(matrix, [(row+i, col+i) for i in range(4)])
+            going_right = is_target(matrix, [(row+i, col+i) for i in range(3)])
+            going_left = is_target(matrix, [(row+i, col+2-i) for i in range(3)])
 
-    return (result, None)
+            if going_right and going_left:
+                result += 1
+
+    return (None, result)
 
 puzzle = Puzzle(2024, 4)
 test_and_submit(puzzle, solution, False)
