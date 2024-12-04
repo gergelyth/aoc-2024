f69242eb1183539dd7d54619c81aacbec4a22465
gergelyth <gergelytothdev@gmail.com>
2024-12-04 23:11:48 +0100

    [Day3] Part 2

--- day3/main.py
+++ day3/main.py
@@ -6,15 +6,32 @@ from core import test_and_submit
 
 
 def solution(input: str) -> tuple[any, any]:
-    pattern = r"mul\((\d{1,3}),(\d{1,3})\)"
+    mul_pattern = r"mul\((\d{1,3}),(\d{1,3})\)"
+    do_pattern = r"do\(\)"
+    dont_pattern = r"don't\(\)"
 
-    matches = re.findall(pattern, input)
+    mul_matches = {match.start(): match for match in re.finditer(mul_pattern, input)}
+    do_matches = {match.start(): match for match in re.finditer(do_pattern, input)}
+    dont_matches = {match.start(): match for match in re.finditer(dont_pattern, input)}
+
+    all_keys = sorted(list(mul_matches.keys()) + list(do_matches.keys()) + list(dont_matches.keys()))
 
     result = 0
-    for (num1, num2) in matches:
-        result += int(num1) * int(num2)
+    mul_enabled = True
+    for i in all_keys:
+        if i in do_matches:
+            mul_enabled = True
+            continue
 
-    return (result, None)
+        if i in dont_matches:
+            mul_enabled = False
+            continue
+
+        if mul_enabled:
+            mul_match = mul_matches[i]
+            result += int(mul_match.group(1)) * int(mul_match.group(2))
+
+    return (None, result)
 
 puzzle = Puzzle(2024, 3)
 test_and_submit(puzzle, solution, False)
