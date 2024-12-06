b7ac687d9181d622bae4a2e52667d6303688698d
gergelyth <gergelytothdev@gmail.com>
2024-12-06 22:08:11 +0100

    [Day6] Part 2

--- day6/main.py
+++ day6/main.py
@@ -1,9 +1,33 @@
 
+from collections import defaultdict
+
 from aocd.models import Puzzle
 
 from core import test_and_submit
 from util import Matrix, add_tuples, directions
 
+
+def is_loop(matrix: Matrix, starting_pos: tuple[int,int]) -> bool:
+    direction_order = ["up", "right", "down", "left"]
+    direction = 0
+
+    visited_positions = defaultdict(set)
+    current = starting_pos
+    while matrix.get_item_pos(current) is not None:
+        if current in visited_positions and direction in visited_positions[current]:
+            return True
+
+        if current != starting_pos:
+            visited_positions[current].add(direction)
+
+        next_pos = add_tuples(current, directions[direction_order[direction]])
+        if matrix.get_item_pos(next_pos) == "#":
+            direction = (direction + 1) % 4
+            continue
+
+        current = next_pos
+
+    return False
 
 def solution(input: str) -> tuple[any, any]:
     matrix = Matrix(input)
@@ -14,23 +38,18 @@ def solution(input: str) -> tuple[any, any]:
                 starting_pos = (row, col)
                 break
 
+    result = 0
+    for row in range(matrix.row_count):
+        for col in range(matrix.col_count):
+            if matrix.get_item(row, col) == ".":
+                matrix.set_item(row, col, "#")
+                print(f"{row * matrix.col_count + col}/{matrix.row_count*matrix.col_count}")
+                if is_loop(matrix, starting_pos):
+                    result += 1
 
-    direction_order = ["up", "right", "down", "left"]
-    direction = 0
+                matrix.set_item(row, col, ".")
 
-    visited_positions = set()
-    current = starting_pos
-    while matrix.get_item_pos(current) is not None:
-        visited_positions.add(current)
-        next_pos = add_tuples(current, directions[direction_order[direction]])
-        if matrix.get_item_pos(next_pos) == "#":
-            direction = (direction + 1) % 4
-            continue
-
-        current = next_pos
-
-    result = len(visited_positions)
-    return (result, None)
+    return (None, result)
 
 puzzle = Puzzle(2024, 6)
 test_and_submit(puzzle, solution, False)
